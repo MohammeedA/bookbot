435edeaa902f3943ac99f9318af7be3d15342f7a
Mohammad Al-Sultani <mohammedalsultani@outlook.com>
2025-04-07 19:14:09 +0200

    modify .gitignore and add get_path_to_book func to main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from stats import get_num_words, get_num_char, get_sorted_list
+import sys
+
+def get_path_to_book():
+    if len(sys.argv) != 2:
+        raise Exception("Usage python3 main.py <path_to_book>")
+    return sys.argv[1]
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,7 +12,11 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    path_to_book = "./books/frankenstein.txt"
+    try:
+        path_to_book = get_path_to_book()
+    except Exception as e:
+        print(e)
+    
     words = get_book_text(path_to_book)
     num_words = get_num_words(words)
     characters = get_num_char(words)
