34cf62c019fc332af4b15ae32f743e8a93640d15
Mohammad Al-Sultani <mohammedalsultani@outlook.com>
2025-04-07 06:03:46 +0200

    add stats.py and rename coun_words func

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+from stats import get_num_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
-def count_words(book_text):
-    num_words = len(book_text.split())
-    return num_words
-
 def main():
-    num_words = count_words(get_book_text("./books/frankenstein.txt"))
+    num_words = get_num_words(get_book_text("./books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(book_text):
+    num_words = len(book_text.split())
+    return num_words
