ddd05b259b3acc179638a0da5ff9484fe993e45c
Mohammad Al-Sultani <mohammedalsultani@outlook.com>
2025-04-07 06:17:03 +0200

    add get_num_char func to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_char
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,7 +6,10 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    num_words = get_num_words(get_book_text("./books/frankenstein.txt"))
+    words = get_book_text("./books/frankenstein.txt")
+    num_words = get_num_words(words)
+    characters = get_num_char(words)
     print(f"{num_words} words found in the document")
+    print(characters)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_num_words(book_text):
     num_words = len(book_text.split())
     return num_words
+
+def get_num_char(words):
+    characters = {}
+    for word in words:
+        for char in word:
+            char = char.lower()
+            if char not in characters:
+                characters[char] = 1
+            else:
+                characters[char] += 1
+    return characters
